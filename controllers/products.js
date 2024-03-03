4bc84fc82b1755a68636913b32e19bf74d169cc3
siva <sivahere9484@gmail.com>
2024-03-03 05:55:30 +0530

    updated-1 controllers and conn.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,44 @@
+const Products=require('../model/productModel')
+
 const getAllProducts=async(req,res)=>{
     try{
-        res.status(200).json({"msg":"These are all products..."})
+        const {company,name,featured,sort,select}=req.query;
+        const queryObject={}
+        if(company){
+            queryObject.company=company 
+        }
+        if(name){
+            queryObject.name={$regex:name,$options:"i"}
+        }
+        if(featured){
+            queryObject.featured=featured
+        }
+        let apiData=Products.find(queryObject)
+        if(sort){
+            let sortFix=sort.replace(","," ")
+            apiData=apiData.sort(sortFix)
+        }
+        if(select){
+            let selectFix=select.split(",").join(" ")
+            apiData=apiData.select(selectFix)
+        }
+        let page=Number(req.query.page)||1
+        let limit=Number(req.query.limit)||3
+        let skip=(page-1)*limit
+        apiData=apiData.skip(skip).limit(limit)
+        const myData=await apiData
+        res.status(200).json({myData})
     }
     catch(err){
+        console.log(err)
         res.status(400).send(err)
     }
 }
 
 const getAllProductsTesting=async(req,res)=>{
     try{
-        res.status(200).json({"msg":"These are all products for testing..."})
+        const myData=await Products.find(req.query)
+        res.status(200).json({myData})
     }
     catch(err){
         res.status(400).send(err)
--- db/conn.js
+++ db/conn.js
@@ -4,4 +4,5 @@ mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log("mongodb connected successfully")
 }).catch((err)=>{
     console.log("Mongodb not connected...")
+    console.log(err)
 })
